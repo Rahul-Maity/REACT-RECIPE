3dc6517ca8ba8368f4cdae58e8344ebc7048af19
Rahul <mrahulmaity623@gmail.com>
2023-09-24 00:19:55 +0530

    fixed the splide

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-
+import { useState,useEffect } from 'react'
+import  { styled }  from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 const Popular = () => {
   const [popular, setPopular] = useState([]);
     useEffect(() => {
@@ -10,24 +11,64 @@ const Popular = () => {
 
     const getPopular=async()=>{
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_SECRET}&number=9`);
-        const data = await api.json();
+      const data = await api.json();
+      console.log(data)
        setPopular(data.recipes)
         
 
     }
   return (
     <div>
-      {
-        popular.map((recipe) => {
-          return (
-            <div key={recipe.id}>
-              <p>{recipe.title}</p>
-            </div>
-          )
-        })
-      }
+      
+       
+        
+      <Wrapper >
+
+        <h3>Popular picks</h3>
+        <Splide options={{
+          perPage:4,
+        }}>
+
+          {popular.map((recipe) => {
+            const words = recipe.title.split(" ");
+            const displayTitle = words.slice(0, 3).join(" ");
+            return (
+              <SplideSlide key={recipe.id}>
+
+                <Card key={recipe.id}>
+               
+                  <p>{displayTitle}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+         
+        
+      
     </div>
-  )
+  );
 }
+
+const Wrapper = styled.div`
+margin:4rem 0rem;
+`;
+const Card = styled.div`
+
+min-height:20rem;
+min-width:20rem;
+padding:3rem;
+overflow:hidden;
+p{
+  overflow:hidden;
+}
+img{
+  width: 21rem;
+  height: 14rem;
+  border-radius: 3rem;
+}
+`;
 
 export default Popular

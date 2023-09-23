2770e06fed769c885ea4ee8c6fe793522a19a684
Rahul <mrahulmaity623@gmail.com>
2023-09-23 20:53:21 +0530

    fetching api data and installing dependencies

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Popular = () => {
+  const [popular, setPopular] = useState([]);
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular=async()=>{
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_SECRET}&number=9`);
+        const data = await api.json();
+       setPopular(data.recipes)
+        
+
+    }
   return (
     <div>
-      popular
+      {
+        popular.map((recipe) => {
+          return (
+            <div key={recipe.id}>
+              <p>{recipe.title}</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
